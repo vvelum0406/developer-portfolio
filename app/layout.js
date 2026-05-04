8d3783dc7c0e6d85b4d7ffc721cf578f4c11a63e
vveluma0406 <varshavelumani1@gmail.com>
2026-05-04 16:38:18 -0400

    final build fix

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+//import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
 import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
